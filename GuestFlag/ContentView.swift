8092bd2ded7dd35d7aa6cd23923026b5b887fbef
Albanna14 <albanna1405@gmail.com>
2021-04-08 13:46:10 +0700

    Initial Commit

---
+++ GuestFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuestFlag
+//
+//  Created by Muhammad Azzam Al Banna on 08/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
